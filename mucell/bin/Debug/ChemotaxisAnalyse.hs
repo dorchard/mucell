ef7581a63ec6b50ea0e94cdfd35b0c57b46375bb
Dominic Orchard <dom.orchard@gmail.com>
2008-12-19 23:21:05 +0000

    deleteoldstuff

--- mucell/bin/Debug/ChemotaxisAnalyse.hs
+++
@@ -1,8 +0,0 @@
-import CSV
-import System.Environment
-
-main = do
-   ~[fname] <- getArgs
-   res <- parseFromFile csvfile fname
-   case res of Left err -> print err
- 	       Right xs -> print xs
